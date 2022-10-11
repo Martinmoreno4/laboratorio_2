6a9460fae57472d63905156c7a2e4fd4608d0565
Martinmoreno4 <68036311+Martinmoreno4@users.noreply.github.com>
2022-10-11 01:52:36 -0300

    Add files via upload

---
+++ ClaseSerializadora.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializacionXml_Herencia
+{
+    public static class ClaseSerializadora<T>
+    {
+        static string ruta;
+        static ClaseSerializadora()
+        {
+            ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//establece ruta de escritorio
+            ruta += @"\Archivos-serializacion";
+        }
+
+        public static void Escribir(T datos, string archivo)
+        {
+            //string completa = ruta + @"\Serializadora_" + archivo + DateTime.Now.ToString("HH_mm_ss") + ".xml";
+            string completa = ruta + @"\Serializadora_" + archivo + ".xml";
+
+            try
+            {
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamWriter sw = new StreamWriter(completa))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                    xmlSerializer.Serialize(sw, datos);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+
+        public static T Leer(string nombre)
+        {
+            string archivo = string.Empty;
+
+            T datos = default;
+
+            try
+            {
+                if (Directory.Exists(ruta))
+                {
+                    string[] archivosEnRuta = Directory.GetFiles(ruta);
+
+                    foreach (string archivoEnRuta in archivosEnRuta)
+                    {
+                        if(archivoEnRuta.Contains(nombre))
+                        {
+                            archivo = archivoEnRuta;
+                            break;
+                        }
+                    }
+
+                    if (archivo != null)
+                    {
+                        using (StreamReader sr = new StreamReader(archivo))
+                        {
+                            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+                            datos = (T)xmlSerializer.Deserialize(sr);//casteo
+                        }
+                    }
+                }
+                return datos;
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {archivo}");
+            }
+        }
+    }
+}
