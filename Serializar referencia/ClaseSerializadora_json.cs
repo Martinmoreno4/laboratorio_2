76747cdd579e1ef93ce35b238e34cedf431891d0
Martinmoreno4 <68036311+Martinmoreno4@users.noreply.github.com>
2022-10-11 01:54:57 -0300

    Add files via upload

---
+++ Serializar referencia/ClaseSerializadora_json.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializacionJson
+{
+    public static class ClaseSerializadora<T>
+    {
+        static string ruta;
+        static ClaseSerializadora()
+        {
+            ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//establece ruta de escritorio
+            ruta += @"\Archivos-serializacion";
+        }
+
+        public static void Escribir(T datos, string archivo)
+        {
+            string completa = ruta + @"\SerializadoraJson_" + archivo + ".json";
+
+            try
+            {
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+                string objetoJson = JsonSerializer.Serialize(datos);
+
+                File.WriteAllText(completa, objetoJson); 
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+
+        public static T Leer(string nombre)
+        {
+            string archivo = string.Empty;
+            string completo = @"\SerializadoraJson_" + nombre + ".json";
+
+            T datos = default;
+
+            try
+            {
+                if (Directory.Exists(ruta))
+                {
+                    string[] archivosEnRuta = Directory.GetFiles(ruta);
+
+                    foreach (string archivoEnRuta in archivosEnRuta)
+                    {
+                        if(archivoEnRuta.Contains(completo))
+                        {
+                            archivo = archivoEnRuta;
+                            break;
+                        }
+                    }
+
+                    if (archivo != null)
+                    {
+                        string archivoJson = File.ReadAllText(archivo);
+                        datos = (T)JsonSerializer.Deserialize<T>(archivoJson);//casteo
+                    }
+                }
+                return datos;
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {archivo}");
+            }
+        }
+    }
+}
---
+++ Serializar referencia/ClaseSerializadora_xml2.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializacionXml
+{
+    
+    public static class ClaseSerializadora
+    {
+        static string ruta;
+        static ClaseSerializadora()
+        {
+            ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//establece ruta de escritorio
+            ruta += @"\Archivos-serializacion";
+        }
+
+        public static void Escribir(Personaje pj)
+        {
+            string completa = ruta + @"\Serializadora_" + DateTime.Now.ToString("HH_mm_ss") + ".xml";
+
+            try
+            {
+                if(!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamWriter sw = new StreamWriter(completa))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Personaje));
+                    xmlSerializer.Serialize(sw, pj);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+
+        public static void EscribirLista(List<Personaje> pjs)
+        {
+            //string completa = ruta + @"\Serializadora_" + DateTime.Now.ToString("HH_mm_ss") + ".xml";
+            string completa = ruta + @"\Serializadora_lista.xml";
+
+            try
+            {
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamWriter sw = new StreamWriter(completa))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Personaje>));
+                    xmlSerializer.Serialize(sw, pjs);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+
+        public static Personaje Leer()
+        {
+            string completa = ruta + @"\Serializadora.xml";
+
+            Personaje pj = null;
+
+            try
+            {
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamReader sr = new StreamReader(completa))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Personaje));
+
+                    pj = (Personaje)xmlSerializer.Deserialize(sr);//casteo
+                }
+
+                return pj;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+
+        public static List<Personaje> LeerLista()
+        {
+            string completa = ruta + @"\Serializadora_lista.xml";
+
+            List<Personaje> pj = null;
+
+            try
+            {
+                if (!Directory.Exists(ruta))
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+
+                using (StreamReader sr = new StreamReader(completa))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Personaje>));
+
+                    pj = (List<Personaje>)xmlSerializer.Deserialize(sr);//casteo
+                }
+
+                return pj;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Error en el archivo {completa}");
+            }
+        }
+    }
+}
